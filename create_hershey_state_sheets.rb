013e526a4230f988914646796cc7f40d5077457b
Chris Williams <chris.d.williams@gmail.com>
2009-06-02 22:18:14 -0400

    First complete cut of the working application.  Need to refactor the pdf code.

---
+++ create_hershey_state_sheets.rb
@@ -0,0 +1,61 @@
+require 'rubygems' 
+require 'prawn'
+require 'prawn/layout'
+require 'parseexcel'
+
+require 'participant'
+require 'hershey_meet_data'
+require 'hershey_pdf'
+
+
+if ARGV[0] != nil
+  workbook = Spreadsheet::ParseExcel.parse(ARGV[0]) 
+
+  # usually, you want the first worksheet: 
+  worksheet = workbook.worksheet(0)
+  
+  hershey_meet_data = HersheyMeetData.new
+  
+  # this will skip the column heading row
+  skip = 1 
+  
+  # indicies for the needed columns
+  age_group_index = 14
+  event_index = 15
+  first_name_index = 16
+  last_name_index = 17
+  community_index = 19
+  time_distance_index = 21
+  
+  test_str = "123 John Smith"
+  puts "Name: " + test_str + " - "+ test_str.inspect
+  
+  worksheet.each(skip) { |row| 
+    name = row.at(first_name_index).to_s('utf-8') + " " + row.at(last_name_index).to_s('utf-8')
+    event = row.at(event_index).to_s('utf-8')
+    age_group = row.at(age_group_index).to_s('utf-8')
+    community = row.at(community_index).to_s('utf-8')
+    time_distance = row.at(time_distance_index).to_s('utf-8')
+    
+    hershey_meet_data.add_participant(name, community, event, age_group)
+  }
+  
+  hershey_pdf = HersheyPdf.new
+  
+  Prawn::Document.generate "table_demo.pdf", :page_layout => :landscape do |pdf|
+    pdf.font "Helvetica"
+    hershey_pdf.pdf = pdf
+    count = 0
+    
+    hershey_meet_data.each_event_as_array do |age_group, event, participants_array|
+      if(count != 0)
+        pdf.start_new_page
+      end
+    
+      hershey_pdf.add_table(age_group, event, participants_array)
+      count += 1
+    end
+  end
+else
+  puts 'Usage: ruby create_hershey_state_sheets.rb <excel file>'
+end
---
+++ hershey_pdf.rb
@@ -0,0 +1,46 @@
+require 'rubygems' 
+require 'prawn'
+require 'prawn/layout'
+
+class HersheyPdf
+  @@field_events = ['Softball Throw', 'Standing Long Jump']
+
+  attr_accessor :pdf
+
+  def initialize
+  end
+
+  def add_table(age_group, event, participants_array)
+    pdf.text "New York West Finals", :size => 32
+    pdf.text "Age Group - " + age_group, :size => 24
+    pdf.text "Event - " + event, :size => 20
+  
+    if @@field_events.include?(event)
+      create_field_event_table(age_group, event, participants_array)
+    else
+      create_running_event_table(age_group, event, participants_array)
+    end
+  end
+
+  private
+
+    def create_running_event_table(age_group, event, participants_array)
+      pdf.table participants_array,
+        :position => :left,
+        :align_headers => :center,
+        :headers => ['Number - Name', 'Community', 'Time', 'Place'],
+        :column_widths => { 0 => 200, 1 => 100, 2 => 90, 3 => 90 },
+        :border_style => :grid,
+        :row_colors => ["FFFFFF", "C0C0C0"]
+    end
+
+    def create_field_event_table(age_group, event, participants_array)
+      pdf.table participants_array,
+        :position => :left,
+        :align_headers => :center,
+        :headers => ['Number - Name', 'Community', 'Distance', 'Distance', 'Distance', 'Place'],
+        :column_widths => { 0 => 200, 1 => 100, 2 => 90, 3 => 90, 4 => 90, 5 => 90},
+        :border_style => :grid,
+        :row_colors => ["FFFFFF", "C0C0C0"]
+    end
+end
