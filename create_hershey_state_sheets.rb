1d9d9b1ddba1167382a51491db5f671906ddef08
Chris Williams <chris.d.williams@gmail.com>
2010-06-12 15:21:42 -0400

    Refactored to spilt the pdf results in to two separate files.  One for field events and one for the running events.

--- create_hershey_state_sheets.rb
+++ create_hershey_state_sheets.rb
@@ -16,20 +16,34 @@ if ARGV[0] != nil
     hershey_meet_data.add_participant(name, community, event, age_group, time_distance)
   end
   
-  hershey_pdf = HersheyPdf.new
+  running_events_pdf = HersheyPdf.new
+  field_events_pdf = HersheyPdf.new
 
-  count = 0
+  running_events_count = 0
+  field_events_count = 0
     
   hershey_meet_data.each_event_as_array do |age_group, event, participants_array|
-    if(count != 0)
-      hershey_pdf.start_new_page
+    if Event.is_field_event?(event)
+      if(field_events_count != 0)
+        field_events_pdf.start_new_page
+      end
+
+      field_events_pdf.add_table(age_group, event, participants_array)
+      field_events_count += 1
+    else
+      if(running_events_count != 0)
+        running_events_pdf.start_new_page
+      end
+
+      running_events_pdf.add_table(age_group, event, participants_array)
+      running_events_count += 1
     end
-    
-    hershey_pdf.add_table(age_group, event, participants_array)
-    count += 1
   end
   
-  hershey_pdf.render_file("test_demo.pdf")
+  basename = File.basename(ARGV[0], ".xls")
+  
+  running_events_pdf.render_file(basename + "_running_events.pdf")
+  field_events_pdf.render_file(basename + "_field_events.pdf")
 else
   puts 'Usage: ruby create_hershey_state_sheets.rb <excel file>'
 end
